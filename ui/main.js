ad9fb427c9c1a483691fd06b937930e321015a95
BIDYASHREEROUT <30669105+BIDYASHREEROUT@users.noreply.github.com>
2017-08-27 14:39:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var element = document.getElementbyId('main-text');
+
+element.innerHTML = 'New Value';
+
+var img = document.getElementbyId('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px'; 
+} ;
